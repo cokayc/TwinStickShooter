fd6fc780146c7988fdaf2666c7bcbb906f776b93
Haakon Larsen <haakon.g.larsen@outlook.com>
2020-02-13 10:17:54 +0100

    twin stick input controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,34 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    public KeyCode shootButton;
     public GameObject bulletPrefab;
+    public float coolDownTime;
+
+
     private Rigidbody2D myRB;
+    private bool canShoot;
     // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
-        
+        canShoot = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         myRB.velocity = speed * new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        if(Input.GetButton(shootButton.ToString()))
+        if(Input.GetButton("Fire1") && canShoot)
         {
+            StartCoroutine(ShotCooldown());
             Instantiate(bulletPrefab, transform.position, transform.rotation);
         }
+    }
+
+    public IEnumerator ShotCooldown()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(coolDownTime);
+        canShoot = true;
     }
 }
