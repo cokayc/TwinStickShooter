46067a1b21b82bba7beadd7f92e7c85bfee0def1
Haakon Larsen <haakon.g.larsen@outlook.com>
2020-02-21 10:59:58 +0100

    merge conflicts begone

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Singleton
+    public static PlayerController instance;
+
     public float speed;
     public GameObject bulletPrefab;
-    public float coolDownTime;
     public float movementThreshold;
 
-    private Rigidbody2D myRB;
-    private bool canShoot;
+    private Rigidbody2D currentRB;
+    private Enemy currentEnemy;
     private int directionMethod;
     private Vector2 pointing;
     private GameManager gm;
+
     // Start is called before the first frame update
     void Start()
     {
-        gm = FindObjectOfType<GameManager>().GetComponent<GameManager>();
-        myRB = GetComponent<Rigidbody2D>();
-        canShoot = true;
-        
+        // Singleton
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+
+        currentRB = GetComponent<Rigidbody2D>();
+        gm = GameManager.instance;
+
         var joysticks = Input.GetJoystickNames();
-        if(joysticks.Length ==0 || joysticks[0].Length == 0)
+        if (joysticks.Length == 0 || joysticks[0].Length == 0)
         {
             directionMethod = 1;
         }
@@ -35,26 +43,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(gm.isPaused)
+        if (gm.isPaused)
         {
             return;
         }
-        myRB.velocity = speed * new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        currentRB.velocity = speed * new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         pointing = determineDirection(pointing);
-        Vector3 bulletSpawnDirection = pointing;
-        if(Input.GetButton("Fire1") && canShoot)
+        if (Input.GetButton("Fire1") && currentEnemy.canShoot)
         {
-            StartCoroutine(ShotCooldown());
-            Instantiate(bulletPrefab, transform.position+bulletSpawnDirection.normalized, transform.rotation).GetComponent<BulletGroup>().direction=pointing;
+            StartCoroutine(currentEnemy.ShotCooldown());
+            Instantiate(bulletPrefab, transform.position, transform.rotation).GetComponent<BulletGroup>().direction = pointing;
 
         }
-    }
-
-    private IEnumerator ShotCooldown()
-    {
-        canShoot = false;
-        yield return new WaitForSeconds(coolDownTime);
-        canShoot = true;
     }
 
     private Vector2 determineDirection(Vector2 directionIn)
@@ -64,29 +65,29 @@ public class PlayerController : MonoBehaviour
         {
             //Mouse
             case 1:
-                Vector2 origin = transform.position;
+                Vector2 origin = currentRB.gameObject.transform.position;
                 Vector2 target = GetMouseWorldPosition();
                 direction = target - origin;
                 direction.Normalize();
                 float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
-                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                currentRB.gameObject.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
                 break;
             //PS4
             case 2:
                 float mouseX = Input.GetAxis("Mouse X");
                 float mouseY = Input.GetAxis("Mouse Y");
-                if (Mathf.Abs(mouseX)>movementThreshold || Mathf.Abs(mouseY)>movementThreshold)
+                if (Mathf.Abs(mouseX) > movementThreshold || Mathf.Abs(mouseY) > movementThreshold)
                 {
                     direction = new Vector2(mouseX, mouseY);
                     direction.Normalize();
-                    
-                    transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg, Vector3.forward);
+
+                    currentRB.gameObject.transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg, Vector3.forward);
                     direction = Vector2.Perpendicular(direction);
                 }
                 break;
             //XBOX
             case 3:
-                break;   
+                break;
         }
         return direction;
     }
@@ -99,5 +100,15 @@ public class PlayerController : MonoBehaviour
         mouseWorldPos.z = 0;
 
         return mouseWorldPos;
+    }
+
+    public void Possess(GameObject target)
+    {
+        if (currentEnemy != null)
+            currentEnemy.isPlayer = false;
+
+        currentRB = target.GetComponent<Rigidbody2D>();
+        currentEnemy = target.GetComponent<Enemy>();
+        currentEnemy.isPlayer = true;
     }
 }
